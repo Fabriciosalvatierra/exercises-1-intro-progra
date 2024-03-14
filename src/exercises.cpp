9c3279069c13f9533ef5fb0371b2ea5f53932033
Ejercicio: Strings, Casting y Control de flujo <124795109+Fabriciosalvatierra@users.noreply.github.com>
2024-03-14 10:08:59 -0400

    prueba 20

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,7 +6,17 @@ using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
   // TODO: YOUR CODE HERE
-  cout << "a" << endl;
+  s1;
+  s2;
+  s3;
+  
+     if (s1 < s2 && s1 < s3) {
+         cout << s1;
+    } else if (s2 < s1 && s2 < s3) {
+         cout << s2;
+    } else {
+        cout << s3;
+    }
 }
 
 void exercise_2(double A, double B, double C) {
