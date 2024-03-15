94f86701c1ec6c179d12b25d9b82447410e1cd1d
Ejercicio: Strings, Casting y Control de flujo <124795109+Fabriciosalvatierra@users.noreply.github.com>
2024-03-15 10:32:08 -0400

    Prueba 24

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -40,31 +40,6 @@ void exercise_5(char character) {
 
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
-  switch (dia) {
-        case 1:
-            cout << "Lunes" << endl;
-            break;
-        case 2:
-            cout << "Martes" << endl;
-            break;
-        case 3:
-            cout << "Miércoles" << endl;
-            break;
-        case 4:
-            cout << "Jueves" << endl;
-            break;
-        case 5:
-            cout << "Viernes" << endl;
-            break;
-        case 6:
-            cout << "Sábado" << endl;
-            break;
-        case 7:
-            cout << "Domingo" << endl;
-            break;
-        default:
-            cout << "Invalid input" << endl;
-    }
 }
 
 void exercise_7(double r) {
