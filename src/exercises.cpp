b44544c2e543a5294a39e91c4d9505e0b17a1ffe
Ejercicio: Strings, Casting y Control de flujo <124795109+Fabriciosalvatierra@users.noreply.github.com>
2024-03-16 22:18:22 -0400

    prueba 45

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -21,71 +21,22 @@ if (s1 < s2 && s1 < s3) {
 
 void exercise_2(double A, double B, double C) {
   // TODO: YOUR CODE HERE
-if (discriminante > 0) {
-        
-        double x1 = (-b + sqrt(discriminante)) / (2 * a);
-        double x2 = (-b - sqrt(discriminante)) / (2 * a);
-        cout << x1 << " " << x2 << ;
-    } else if (discriminante == 0) {
-        // Una raíz real
-        double x = -b / (2 * a);
-        cout << x << ;
-    } else {
-        
-        cout << "No hay raíces reales" << ;
-    }
+
 }
 
 void exercise_3(int a, int b) {
   // TODO: YOUR CODE HERE
-    int A, B;
-    cin >> A >> B;
-
-    if (B == 0) {
-        cout << "Imposible" << endl;
-    } else {
-        cout << A / B << endl;
-    }
-
+  
 }
 
 void exercise_4(double n, double a, double b, double x, double y) {
   // TODO: YOUR CODE HERE
-cout << "Ingrese el costo original del producto (N): ";
-    cin >> N;
-    cout << "Ingrese el valor de A: ";
-    cin >> A;
-    cout << "Ingrese el valor de B: ";
-    cin >> B;
-    cout << "Ingrese el descuento X (%): ";
-    cin >> X;
-    cout << "Ingrese el descuento Y (%): ";
-    cin >> Y;
-    
-    if (N > A) { 
-        N -= N * (X / 100.0);
-    }
-    if (N > B) {
-        N -= N * (Y / 100.0);
-    }
-    cout << "El costo de compra del producto con descuento es: " << N << endl;
     
 }
 
 void exercise_5(char character) {
   // TODO: YOUR CODE HERE
-char caracter;
     
-    cout << "Ingrese un caracter: ";
-    cin >> caracter;
-    
-    if (caracter >= 'A' && caracter <= 'Z') {
-        cout << "El caracter ingresado es una letra mayuscula." << endl;
-    } else if (caracter >= 'a' && caracter <= 'z') {
-        cout << "El caracter ingresado es una letra minuscula." << endl;
-    } else {
-        cout << "El caracter ingresado no es una letra." << endl;
-    }
 }
 
 void exercise_6(int number) {
@@ -98,6 +49,7 @@ void exercise_7(double r) {
 
 void exercise_8(long int seconds) {
   // TODO: YOUR CODE HERE
+
 }
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
